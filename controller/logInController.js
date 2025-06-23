b0994cc350c695f5ec9a5b8d7b815fc4494c7338
ShraddhaMulekar <mulekarshraddha@gmail.com>
2025-06-23 23:55:06 +0530

    log in functionality ready!

---
+++ controller/logInController.js
@@ -0,0 +1,32 @@
+import UserModel from "../models/user.model.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const logInController = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const matchEmail = await UserModel.findOne({ email });
+    if (!matchEmail) {
+      return res.json({ msg: "email id not registered. please sign in now!" });
+    } else {
+      bcrypt.compare(password, matchEmail.password, async (err, result) => {
+        if (err) {
+          return res.json({ msg: "error here", err });
+        } else if (result) {
+          const payload = {
+            userId: matchEmail.id,
+            userName: matchEmail.userName,
+          };
+          const token = jwt.sign(payload, process.env.SECREATEKEY);
+          return res.json({ msg: "Log in successful!", token });
+        } else {
+          return res.json({ msg: "invalid password.." });
+        }
+      });
+    }
+  } catch (error) {
+    return res.json({ msg: "error in log in controller!", error });
+  }
+};
+
+export default logInController;
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,8 +1,10 @@
 import express from "express"
 import signInController from "../controller/signInController.js"
+import logInController from "../controller/logInController.js"
 
 const userRouter = express.Router()
 
 userRouter.post("/signin", signInController)
+userRouter.post("/login", logInController)
 
 export default userRouter
